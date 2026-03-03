d5b339aaa98a6bed05c000b5ad9a31d39cdfe43d
Prashuk Jain <prashukjain7722@gmail.com>
2026-03-03 20:14:59 +0530

    full woring with address update and change addres feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,27 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
 urlpatterns = [
 
+    # AUTH
+    path('signup/', views.signup, name='signup'),
+
+    path('login/', auth_views.LoginView.as_view(
+        template_name='accounts/login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/'
+    ), name='logout'),
+
+    # PROFILE
     path('profile/', views.profile, name='profile'),
 
+    # ADDRESS
     path('add-address/', views.add_address, name='add_address'),
-
     path('edit-address/<int:id>/', views.edit_address, name='edit_address'),
-
     path('delete-address/<int:id>/', views.delete_address, name='delete_address'),
-
 ]
--- orders/models.py
+++ orders/models.py
@@ -16,7 +16,15 @@ class CartItem(models.Model):
         return self.product.price * self.quantity
     
 class Order(models.Model):
-    user = models.CharField(max_length=200)
+
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     address = models.ForeignKey(
         Address,
@@ -25,12 +33,18 @@ class Order(models.Model):
         blank=True
     )
 
-    total_amount = models.IntegerField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
-    status = models.CharField(max_length=20, default='Pending')
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='Pending'
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.user.username}"
 class OrderItem(models.Model):
 
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
